a262fe82a1747d3e54056d32c66bc80c004bbc1e
Riyaarana <106746892+Riyaarana@users.noreply.github.com>
2022-06-04 09:53:42 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,12 @@
+x=int(input("enter first num:"))
+y=int(input("enter second num:"))
+z=x*y
+print("multiplication result ",z)
+sum=x+y
+print("sum is ",sum)
+subtraction =x-y
+print("difference is ",subtraction)
+division=x/y
+print("quotient is ",division)
+rem=x%y
+print("remainder is",rem)
