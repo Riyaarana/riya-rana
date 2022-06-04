13109fba430685c7c6abd13d288e3392c732bd39
Riyaarana <106746892+Riyaarana@users.noreply.github.com>
2022-06-04 10:11:37 +0530

    Create 43.py

---
+++ 43.py
@@ -0,0 +1,9 @@
+maths=float(input("enter maths marks:"))
+chem=float(input("enter chemistry marks:"))
+bio=float(input("enter bio marks:"))
+eng=float(input("enter english marks:"))
+hin=float(input("enter hindi marks:"))
+avg=(maths+chem+bio+eng+hin)/5
+per=(maths+chem+bio+eng+hin)*100/500
+print("total average:",avg)
+print("Percentage = ",per)
