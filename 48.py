62e08feb0418e328951ddddb27c63ca939c92392
Riyaarana <106746892+Riyaarana@users.noreply.github.com>
2022-06-06 16:09:49 +0530

    Create 48.py

---
+++ 48.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    return x+y;
+def sub(x,y):
+    return x-y;
+def mul(x,y):
+    return x*y;
+def devide(x,y):
+    return x/y;
+def rem(x,y):
+    return x%y;
+num1=int(input("enter first num:"))
+num2=int(input("enter second num:"))
+print("sum of num is",add(num1,num2))
+print("sub of num is",sub(num1,num2))
+print("multiplication of num is",mul(num1,num2))
+print("division of num is",devide(num1,num2))
+print("remainder of num is",rem(num1,num2))
