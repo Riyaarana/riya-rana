296854b3cbc697899f0ea3b5a70a262e16117715
Riyaarana <106746892+Riyaarana@users.noreply.github.com>
2022-06-07 10:52:30 +0530

    Create 88.py

---
+++ 88.py
@@ -0,0 +1,5 @@
+char=input("enter the character:")
+if(char=='a'or char=='e'or char=='i'or char=='o'or char=='u'or char=='A'or char=='E'or char=='I'or char=='O'or char=='U'):
+     print("its vowel")
+else:
+    print("its a consonent")
