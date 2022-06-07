16dd882f53e6b9bb22b90441aa8eab2242aedb33
Riyaarana <106746892+Riyaarana@users.noreply.github.com>
2022-06-07 10:59:03 +0530

    Create 100.py

---
+++ 100.py
@@ -0,0 +1,11 @@
+str=input("enter the string:")
+count=0
+count2=0
+for i in range(0,len(str)):
+    if(str[i]=='a' or str[i]=='e' or str[i]=='i' or str[i]=='o' or str[i]=='u' 
+    or str[i]=='A' or str[i]=='E' or str[i]=='I' or str[i]=='O' or str[i]=='U'):
+            count=count+1
+    else:
+        count2=count2+1
+print("total number of vowels in string:",count)
+print("total number of consonent in string:",count2)
