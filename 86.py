6b0c947e6d82411091428cd400d292e0f38544b9
Riyaarana <106746892+Riyaarana@users.noreply.github.com>
2022-06-07 10:50:51 +0530

    Create 86.py

---
+++ 86.py
@@ -0,0 +1,7 @@
+char=input("enter the character:")
+if(char>='A'and char<='Z'):
+    print("its uppercase char")
+elif(char>='a'and char<='z'):
+    print("its not uppercase char")
+else:
+    print("its not uppercase char")
