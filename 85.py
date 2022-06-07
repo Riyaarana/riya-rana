c6f573773850aa5e47fd6688ab8b6ca92486ab60
Riyaarana <106746892+Riyaarana@users.noreply.github.com>
2022-06-07 10:50:21 +0530

    Create 85.py

---
+++ 85.py
@@ -0,0 +1,7 @@
+char=input("enter the character:")
+if(char>='a'and char<='z'):
+    print("its lowercase char")
+elif(char>='A'and char<='Z'):
+    print("its not lowercase char")
+else:
+    print("its not lowercase char")
