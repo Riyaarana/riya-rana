1421575a4304c58c0c805700ec7a3845c36a6e57
Riyaarana <106746892+Riyaarana@users.noreply.github.com>
2022-06-07 10:49:34 +0530

    Create 83.py

---
+++ 83.py
@@ -0,0 +1,7 @@
+char=input("enter the character:")
+if((char>='a'and char<='z')or(char>='A'and char<='Z')):
+    print("its an alphabet")
+elif(char>='0'and char<='9'):
+    print("its a digit")
+else:
+    print("its special character")
